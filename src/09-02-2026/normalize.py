990865a6e6c8cfbf442602e46419caaca60c3011
darshanhm03 <darshanmgowda03@gmail.com>
2026-02-09 16:21:39 +0530

    Initial commit

---
+++ src/09-02-2026/normalize.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+np.random.seed(42)  
+scores = np.random.randint(50, 101, size=(5, 3))
+
+subject_means = scores.mean(axis=0)
+
+centered_scores = scores - subject_means
+
+print("Original Scores:")
+print(scores)
+
+print("\nSubject-wise Mean:")
+print(subject_means)
+
+print("\nCentered (Normalized) Scores:")
+print(centered_scores)
